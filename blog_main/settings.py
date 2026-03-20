78573f84e683da65a5ee5dc9ab3c4f68ddfa7f8a
chrizmeza <angelariasmezacrack@gmail.com>
2026-03-20 00:20:40 -0700

    nuevos agustes a django

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'blog'
 ]
 
 ROOT_URLCONF = 'blog_main.urls'
