49a7fd6856a091518288ba0bca90c529bf3bbe80
Cesaraarr <arturocesar521@gmail.com>
2026-04-17 15:18:01 -0700

    cambios en forms

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,19 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+        windgets = { 
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'tu nombre'
+            }),
+            'email': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 1,
+                'placeholder': 'tu email'
+            }),
+              'body': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 6,
+                'placeholder': 'Escribe tu comentario'
+            }),
+        }
